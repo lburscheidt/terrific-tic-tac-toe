a515e3aec31bf67062af370ce41c03e3c9231aed
lburscheidt <176681558+lburscheidt@users.noreply.github.com>
2024-09-07 17:50:38 +0200

    Add function "move" that replaces a number with x or o

--- script.js
+++ script.js
@@ -1,20 +1,43 @@
-function createGameboard() {
+/*function createGameboard() {
   var num = '123456789';
   var row = 3; // Known at run time
   var col = 3; // Known at run time
   var i = 0;
 
-  var array2D = [[]];
-  for (var r = 0; r < row; ++r) {
-    array2D[r] = [];
-    for (var c = 0; c < col; ++c) {
-      array2D[r][c] = num[i++];
+
     }
+  }*/
+let i = 0;
+let num = '123456789';
+let gameboard = [[]];
+for (let r = 0; r < 3; ++r) {
+  gameboard[r] = [];
+  for (let c = 0; c < 3; ++c) {
+    gameboard[r][c] = num[i++];
   }
-  console.log(array2D);
 }
-createGameboard();
+
+console.table(gameboard);
+
+function move(token) {
+  let moveRow = prompt('Row');
+  let moveColumn = prompt('Column');
+  gameboard[moveRow][moveColumn] = token;
+  console.table(gameboard);
+}
+
+move('x');
 
 function createPlayer() {}
 
-function createDisplayController() {}
+function createDisplayController() {
+  /*if (
+      gameboard[moveRow][moveColumn] !== 'o' &&
+      gameboard[moveRow][moveColumn] !== 'x'
+    ) {
+      gameboard[moveRow][moveColumn] = token;
+    }
+    else{
+
+    }*/
+}
