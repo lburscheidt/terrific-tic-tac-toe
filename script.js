9d086e866e5c359db15d0f918a0b115913fb4f99
lburscheidt <176681558+lburscheidt@users.noreply.github.com>
2024-09-07 19:31:01 +0200

    Refactor gameboard creation function into IIFE

--- script.js
+++ script.js
@@ -1,36 +1,31 @@
-/*function createGameboard() {
-  var num = '123456789';
-  var row = 3; // Known at run time
-  var col = 3; // Known at run time
-  var i = 0;
-
-
+const gameboard = (function () {
+  let makeBoard = [[]];
+  for (let r = 0; r < 3; ++r) {
+    makeBoard[r] = [];
+    for (let c = 0; c < 3; ++c) {
+      makeBoard[r][c] = '0';
     }
-  }*/
-let i = 0;
-let num = '123456789';
-let gameboard = [[]];
-for (let r = 0; r < 3; ++r) {
-  gameboard[r] = [];
-  for (let c = 0; c < 3; ++c) {
-    gameboard[r][c] = num[i++];
   }
-}
+  return { makeBoard };
+})();
 
-console.table(gameboard);
+console.table(gameboard.makeBoard);
 
+/*
 function move(token) {
+  let board = createGameboard();
+  console.table(board);
   let moveRow = prompt('Row');
   let moveColumn = prompt('Column');
-  gameboard[moveRow][moveColumn] = token;
-  console.table(gameboard);
+  board[moveRow][moveColumn] = token;
+  console.table(board);
 }
 
-move('x');
+function createPlayer(name, token) {
+  return { name, token };
+}
 
-function createPlayer() {}
-
-function createDisplayController() {
+/*function createDisplayController() {
   /*if (
       gameboard[moveRow][moveColumn] !== 'o' &&
       gameboard[moveRow][moveColumn] !== 'x'
@@ -39,5 +34,9 @@ function createDisplayController() {
     }
     else{
 
-    }*/
+    }
 }
+*/ /*
+createPlayer('Max', 'x');
+createPlayer('Otto', 'o');
+/*move('x');*/
