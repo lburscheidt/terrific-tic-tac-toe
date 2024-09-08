a6a5c3ee7f2a34acf4de84c723e34089571f1748
lburscheidt <176681558+lburscheidt@users.noreply.github.com>
2024-09-08 19:08:31 +0200

    DOM version: working version with letters x and o

--- dom-version/script.js
+++ dom-version/script.js
@@ -1,12 +1,12 @@
 const tictactoe = (function () {
   const gameboard = (function () {
-    let makeBoard = document.querySelector(".gameboard")
+    let makeBoard = document.querySelector('.gameboard')
     for (let i = 0; i < 3; i++) {
       for (let j = 0; j < 3; j++) {
-        const newCell = document.createElement("button")
-        newCell.classList.add("cell")
-        newCell.setAttribute("id", `${i}${j}`)
-        newCell.addEventListener("click", function () {
+        const newCell = document.createElement('button')
+        newCell.classList.add('cell')
+        newCell.setAttribute('id', `${i}${j}`)
+        newCell.addEventListener('click', function () {
           move(newCell.id)
         })
         makeBoard.appendChild(newCell)
@@ -14,15 +14,15 @@ const tictactoe = (function () {
     }
     return { makeBoard }
   })()
-  let _1 = document.getElementById("00")
-  let _2 = document.getElementById("01")
-  let _3 = document.getElementById("02")
-  let _4 = document.getElementById("10")
-  let _5 = document.getElementById("11")
-  let _6 = document.getElementById("12")
-  let _7 = document.getElementById("20")
-  let _8 = document.getElementById("21")
-  let _9 = document.getElementById("22")
+  let _1 = document.getElementById('00')
+  let _2 = document.getElementById('01')
+  let _3 = document.getElementById('02')
+  let _4 = document.getElementById('10')
+  let _5 = document.getElementById('11')
+  let _6 = document.getElementById('12')
+  let _7 = document.getElementById('20')
+  let _8 = document.getElementById('21')
+  let _9 = document.getElementById('22')
 
   function createPlayer(name, token) {
     return { name, token }
@@ -31,9 +31,7 @@ const tictactoe = (function () {
   function hasWon(player) {
     let t = player.token
     if (
-      (_1.innerHTML === player.token &&
-        _2.innerHTML === player.token &&
-        _3.innerHTML === player.token) ||
+      (_1.innerHTML === t && _2.innerHTML === t && _3.innerHTML === t) ||
       (_4.innerHTML === t && _5.innerHTML === t && _6.innerHTML === t) ||
       (_7.innerHTML === t && _8.innerHTML === t && _9.innerHTML === t) ||
       (_1.innerHTML === t && _4.innerHTML === t && _7.innerHTML === t) ||
@@ -145,11 +143,7 @@ const tictactoe = (function () {
       isTied() === false
     ) {
       cell.innerHTML = currentPlayer.token
-      cell.removeEventListener("click", function () {
-        move(cell.id)
-      })
       cell.disabled = true
-      console.log(cell.innerHTML)
       if (currentPlayer === firstPlayer) {
         currentPlayer = secondPlayer
         break
@@ -161,10 +155,10 @@ const tictactoe = (function () {
     gameEnds()
   }
 
-  let x_token = "x"
-  let o_token = "o"
-  let firstPlayer = createPlayer("Max", x_token)
-  let secondPlayer = createPlayer("Otto", o_token)
+  let x_token = 'x' /*'<i class="fa-solid fa-x"></i>'*/
+  let o_token = 'o' /*'<i class="fa-solid fa-o"></i>'*/
+  let firstPlayer = createPlayer('Max', x_token)
+  let secondPlayer = createPlayer('Otto', o_token)
   let currentPlayer = firstPlayer
 
   function gameEnds() {
