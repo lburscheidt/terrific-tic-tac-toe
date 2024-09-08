f07166247eec766263a998d343ae47598c5705e1
lburscheidt <176681558+lburscheidt@users.noreply.github.com>
2024-09-08 10:28:04 +0200

    Clear console after each move

--- script.js
+++ script.js
@@ -14,57 +14,60 @@ function createPlayer(name, token) {
   return { name, token }
 }
 
-let board = gameboard.makeBoard
+const gameFlow = (function () {
+  let board = gameboard.makeBoard
 
-let firstPlayer = createPlayer("Max", "x")
-let secondPlayer = createPlayer("Otto", "o")
+  let firstPlayer = createPlayer("Max", "x")
+  let secondPlayer = createPlayer("Otto", "o")
 
-console.table(board)
-
-while (hasWon(firstPlayer) == false && hasWon(secondPlayer) == false) {
-  move(firstPlayer)
-  console.log(`${firstPlayer.name} has placed a token.`)
-  move(secondPlayer)
-  console.log(`${secondPlayer.name} has placed a token.`)
-}
-winningMessage()
-
-function move(player) {
-  let moveRow = prompt(`${player.name}, pick a row`)
-  let moveColumn = prompt(`${player.name}, pick a column`)
-  board[moveRow][moveColumn] = player.token
   console.table(board)
-}
-
-function hasWon(player) {
-  if (
-    (board[0][0] === player.token &&
-      board[0][1] === player.token &&
-      board[0][2] === player.token) ||
-    (board[1][0] === player.token &&
-      board[1][1] === player.token &&
-      board[1][2] === player.token) ||
-    (board[2][0] === player.token &&
-      board[2][1] === player.token &&
-      board[2][2] === player.token) ||
-    (board[0][0] === player.token &&
-      board[1][0] === player.token &&
-      board[2][0] === player.token) ||
-    (board[1][0] === player.token &&
-      board[1][1] === player.token &&
-      board[1][2] === player.token) ||
-    (board[2][0] === player.token &&
-      board[2][1] === player.token &&
-      board[2][2] === player.token) ||
-    (board[0][0] === player.token &&
-      board[1][1] === player.token &&
-      board[2][2] === player.token) ||
-    (board[0][2] === player.token &&
-      board[1][1] === player.token &&
-      board[2][0] === player.token)
-  ) {
-    return true
-  } else {
-    return false
+  while (hasWon(firstPlayer) == false && hasWon(secondPlayer) == false) {
+    move(firstPlayer)
+    console.log(`${firstPlayer.name} has placed a token.`)
+    move(secondPlayer)
+    console.log(`${secondPlayer.name} has placed a token.`)
   }
-}
+  console.log("Game over")
+  /*winningMessage()*/
+
+  function move(player) {
+    let moveRow = prompt(`${player.name}, pick a row`)
+    let moveColumn = prompt(`${player.name}, pick a column`)
+    console.clear()
+    board[moveRow][moveColumn] = player.token
+    console.table(board)
+  }
+
+  function hasWon(player) {
+    if (
+      (board[0][0] === player.token &&
+        board[0][1] === player.token &&
+        board[0][2] === player.token) ||
+      (board[1][0] === player.token &&
+        board[1][1] === player.token &&
+        board[1][2] === player.token) ||
+      (board[2][0] === player.token &&
+        board[2][1] === player.token &&
+        board[2][2] === player.token) ||
+      (board[0][0] === player.token &&
+        board[1][0] === player.token &&
+        board[2][0] === player.token) ||
+      (board[1][0] === player.token &&
+        board[1][1] === player.token &&
+        board[1][2] === player.token) ||
+      (board[2][0] === player.token &&
+        board[2][1] === player.token &&
+        board[2][2] === player.token) ||
+      (board[0][0] === player.token &&
+        board[1][1] === player.token &&
+        board[2][2] === player.token) ||
+      (board[0][2] === player.token &&
+        board[1][1] === player.token &&
+        board[2][0] === player.token)
+    ) {
+      return true
+    } else {
+      return false
+    }
+  }
+})()
