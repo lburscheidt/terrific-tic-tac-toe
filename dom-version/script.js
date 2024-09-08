ada2fc588a122c210521065c3215538a63deff9e
lburscheidt <176681558+lburscheidt@users.noreply.github.com>
2024-09-08 15:46:35 +0200

    DOM version: add game flow functions hasWon and move

--- dom-version/script.js
+++ dom-version/script.js
@@ -20,7 +20,45 @@ const gameboard = (function () {
 function createPlayer(name, token) {
   return { name, token }
 }
-
 let firstPlayer = createPlayer("Max", "<i class='fa-solid fa-x fa-10x'></i>")
 let secondPlayer = createPlayer("Otto", "<i class='fa-solid fa-o fa-10x'></i>")
+let currentPlayer = firstPlayer
 //game flow
+
+function hasWon(currentPlayer) {
+  if (document.getElementById("00").innerHTML === currentPlayer.token) {
+    return true
+  } else {
+    return false
+  }
+}
+
+function isTied() {
+  if (document.getElementById("01").innerHTML === secondPlayer.token) {
+    return true
+  } else {
+    return false
+  }
+}
+
+function move(cellId) {
+  while (hasWon(firstPlayer) === false && hasWon(secondPlayer) === false) {
+    let cell = document.getElementById(cellId)
+    cell.innerHTML = currentPlayer.token
+    if (hasWon(firstPlayer) || hasWon(secondPlayer)) {
+      if (hasWon(firstPlayer)) {
+        console.log(`${firstPlayer.name} has won`)
+      } else if (hasWon(secondPlayer)) {
+        console.log(`${secondPlayer.name} has won`)
+        break
+      }
+      if (currentPlayer === firstPlayer) {
+        currentPlayer = secondPlayer
+        break
+      } else if (currentPlayer === secondPlayer) {
+        currentPlayer = firstPlayer
+        break
+      }
+    }
+  }
+}
